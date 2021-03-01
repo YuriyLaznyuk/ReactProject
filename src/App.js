03644abd098971d32489fc720d1efc10219154a0
YuriyLaznyuk <yuriy.laznyuk@gmail.com>
2021-03-02 01:42:16 +0200

    react router

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
 import './App.css';
 import React, {Component} from 'react';
 import {MainMenu} from "./components/MainMenu";
-import LayoutContainer from "./components/LayoutContainer";
+import {BrowserRouter} from "react-router-dom";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            layout: '',
-            products: [],
-        }
-    }
 
-    onChangeLayout = (layout) => {
-        this.setState({
-            layout: layout
-        });
-    }
 
     render() {
 
         return (
-            <div>
+            <BrowserRouter>
                 <MainMenu
-                    onChangeLayout={this.onChangeLayout}
-                    layout={this.state.layout}
+
                 />
-                <LayoutContainer
-                    layout={this.state.layout}
-                    products={this.state.products}
-                />
-            </div>
+
+            </BrowserRouter>
         );
     }
 }
