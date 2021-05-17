9f0c3fe74b99117d744f95863f1447467abbf33a
YuriyLaznyuk <yuriy.laznyuk@gmail.com>
2021-05-17 18:23:38 +0300

    active link

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -11,34 +11,36 @@ export function MainMenu(props) {
         <div>
             <ul className='MainMenu_ul'>
                 <li className={useRouteMatch("/admin") ? 'red' : "blue"}>
-                    <Link to='/admin'>
+
+                    <Link to='/admin' >
                         Admin
                     </Link>
                 </li>
 
-                <li className={useRouteMatch("/usecontainer") ? 'red' : 'blue'}>
-                    <Link to='/usecontainer'>
+                <li className= 'blue'>
+
+                    <Link to='/' exact>
                         UserContainer
                     </Link>
                 </li>
 
                 <li className={useRouteMatch("/container1") ? 'red' : 'blue'}>
 
-                    <Link to='/container1'>
+                    <Link to='/container1' >
                         Container 1
                     </Link>
                 </li>
 
                 <li className={useRouteMatch("/container2") ? 'red' : 'blue'}>
 
-                    <Link to='/container2'>
+                    <Link to='/container2' >
                         Container 2
                     </Link>
                 </li>
             </ul>
 
             <Switch>
-                <Route exact path='/usecontainer'><UserContainer/></Route>
+                <Route exact path='/'><UserContainer/></Route>
                 <Route path='/container1'><h1>Container 1</h1></Route>
                 <Route path='/container2'><h1>Container 2</h1></Route>
                 <Route path="/login"><Login/></Route>
